7bdd71d8934e13ff5a69cfafd2bf7ec0ed38d2f5
Danish Ansari <dvansari360@gmail.com>
2025-12-12 10:48:42 +0530

    feat:created socket context provider

---
+++ frontend/providers/SocketProvider.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { initialiseSocket } from "@/lib/socket";
+import { ReactNode, useContext, useEffect } from "react";
+import { createContext } from "react";
+
+
+interface socketProviderProps {
+    children:ReactNode
+}
+
+const SocketContext = createContext<ReturnType<typeof initialiseSocket> | null>(null)
+
+export const SocketProvider = ({children}:socketProviderProps)=>{
+    const socket = initialiseSocket()
+    useEffect(()=>{
+        socket.connect();
+        return ()=>{
+            socket.disconnect()
+        }
+    },[])
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
+
+export const useSocket = ()=>{
+    const socket = useContext(SocketContext)
+    if(!socket){
+        throw new Error("useSocket must be used within SocketProvide")
+    }
+    return socket;
+}
