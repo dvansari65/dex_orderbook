adf91bc1353d37b7672f4e9a6556e4852e23134f
Danish Ansari <dvansari360@gmail.com>
2025-12-12 10:48:00 +0530

    feat:created seperate folder for provider

---
+++ frontend/components/app-provider.tsx
@@ -0,0 +1,12 @@
+import { SocketProvider } from "@/providers/SocketProvider";
+import { ReactNode } from "react";
+
+
+
+export const  AppProvider = ({children}:{children:ReactNode})=>{
+    return (
+        <SocketProvider>
+            {children}
+        </SocketProvider>
+    )
+}
