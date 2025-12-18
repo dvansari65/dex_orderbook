81452a1c4d9c55417b99013577e2ac576bfd1f41
Danish Ansari <dvansari360@gmail.com>
2025-12-18 12:39:57 +0530

    feat:implemented two fns on Side

---
+++ smart_contract/programs/orderbook/states/order_schema/enums.rs
@@ -0,0 +1,20 @@
+#[derive(AnchorDeserialize,AnchorSerialize,Clone,Copy,PartialEq,Eq,InitSpace,Debug)]
+pub enum Side {
+    Bid,
+    Ask
+}
+
+impl Side {
+    pub fn opposite (&self)->Self{
+        match *self {
+            Side::Bid => Side::Ask,
+            Side::Ask => Side::Bid
+        }
+    }
+    pub fn from_order_sequence_number (order_id:&u64)->Self{
+        match order_id.leading_zeros(){
+            1 => Side::bid,
+            _ => Side::Ask
+        }
+    }
+}
