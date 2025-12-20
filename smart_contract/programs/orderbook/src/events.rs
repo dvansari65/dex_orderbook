4cb9995e7af287469da87af3da5f2770a81de19a
Danish Ansari <dvansari360@gmail.com>
2025-12-20 13:54:40 +0530

    feat:added events and helper functions to emit the events

--- smart_contract/programs/orderbook/src/events.rs
+++ smart_contract/programs/orderbook/src/events.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
-
 use crate::states::order_schema::enums::Side;
- 
+
+// Events for indexers
 #[event]
 pub struct OrderPlacedEvent {
     pub market: Pubkey,
@@ -10,42 +10,50 @@ pub struct OrderPlacedEvent {
     pub client_order_id: u64,
     pub side: Side,
     pub price: u64,
-    /// Base lots placed on book
     pub base_lots: u64,
     pub timestamp: i64,
 }
 
 #[event]
 pub struct OrderFillEvent {
-    /// Maker (liquidity provider)
     pub maker: Pubkey,
     pub maker_order_id: u64,
-    /// Taker (liquidity remover)
     pub taker: Pubkey,
+    pub taker_order_id: u64,
     pub side: Side,
     pub price: u64,
-    /// Filled in this match
     pub base_lots_filled: u64,
-    /// Remaining on maker order
+    pub base_lots_remaining: u64,
+    pub timestamp: i64,
+}
+#[event]
+pub struct OrderPartialFillEvent {
+    pub maker: Pubkey,
+    pub maker_order_id: u64,
+    pub taker: Pubkey,
+    pub taker_order_id: u64,
+    pub side: Side,
+    pub price: u64,
+    pub base_lots_filled: u64,
     pub base_lots_remaining: u64,
     pub timestamp: i64,
 }
 
 #[event]
 pub struct OrderReducedEvent {
+    pub market: Pubkey,
     pub owner: Pubkey,
     pub order_id: u64,
     pub side: Side,
     pub price: u64,
-    /// Lots removed
     pub base_lots_removed: u64,
-    /// Remaining after reduce
     pub base_lots_remaining: u64,
     pub timestamp: i64,
 }
 
 #[event]
 pub struct OrderCancelledEvent {
+    pub market: Pubkey,
     pub owner: Pubkey,
     pub order_id: u64,
     pub side: Side,
@@ -53,10 +61,9 @@ pub struct OrderCancelledEvent {
     pub timestamp: i64,
 }
 
-// -------------------- SPECIAL EVENTS -------------------- //
-
 #[event]
 pub struct OrderEvictedEvent {
+    pub market: Pubkey,
     pub owner: Pubkey,
     pub order_id: u64,
     pub side: Side,
@@ -67,6 +74,7 @@ pub struct OrderEvictedEvent {
 
 #[event]
 pub struct OrderExpiredEvent {
+    pub market: Pubkey,
     pub owner: Pubkey,
     pub order_id: u64,
     pub side: Side,
@@ -77,6 +85,7 @@ pub struct OrderExpiredEvent {
 
 #[event]
 pub struct FeeCollectedEvent {
+    pub market: Pubkey,
     pub owner: Pubkey,
     pub order_id: u64,
     pub fees_collected_in_quote_lots: u64,
@@ -85,7 +94,180 @@ pub struct FeeCollectedEvent {
 
 #[event]
 pub struct TimeInForceEvent {
+    pub market: Pubkey,
     pub order_id: u64,
     pub last_valid_slot: u64,
     pub last_valid_unix_timestamp_in_seconds: u64,
+}
+
+// Helper functions for emitting events
+pub fn emit_order_placed(
+    market: Pubkey,
+    owner: Pubkey,
+    order_id: u64,
+    client_order_id: u64,
+    side: Side,
+    price: u64,
+    base_lots: u64,
+) {
+    emit!(OrderPlacedEvent {
+        market,
+        owner,
+        order_id,
+        client_order_id,
+        side,
+        price,
+        base_lots,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+
+pub fn emit_order_fill(
+    maker: Pubkey,
+    maker_order_id: u64,
+    taker: Pubkey,
+    taker_order_id: u64,
+    side: Side,
+    price: u64,
+    base_lots_filled: u64,
+    base_lots_remaining: u64,
+) {
+    emit!(OrderFillEvent {
+        maker,
+        maker_order_id,
+        taker,
+        taker_order_id,
+        side,
+        price,
+        base_lots_filled,
+        base_lots_remaining,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+pub fn emit_partial_fill_order (
+    maker: Pubkey,
+    maker_order_id: u64,
+    taker: Pubkey,
+    taker_order_id: u64,
+    side: Side,
+    price: u64,
+    base_lots_filled: u64,
+    base_lots_remaining: u64,
+) {
+    emit!(OrderPartialFillEvent {
+        maker,
+        maker_order_id,
+        taker,
+        taker_order_id,
+        side,
+        price,
+        base_lots_filled,
+        base_lots_remaining,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    })
+}
+
+pub fn emit_order_cancelled(
+    market: Pubkey,
+    owner: Pubkey,
+    order_id: u64,
+    side: Side,
+    price: u64,
+) {
+    emit!(OrderCancelledEvent {
+        market,
+        owner,
+        order_id,
+        side,
+        price,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+
+pub fn emit_order_reduced(
+    market: Pubkey,
+    owner: Pubkey,
+    order_id: u64,
+    side: Side,
+    price: u64,
+    base_lots_removed: u64,
+    base_lots_remaining: u64,
+) {
+    emit!(OrderReducedEvent {
+        market,
+        owner,
+        order_id,
+        side,
+        price,
+        base_lots_removed,
+        base_lots_remaining,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+
+pub fn emit_order_evicted(
+    market: Pubkey,
+    owner: Pubkey,
+    order_id: u64,
+    side: Side,
+    price: u64,
+    base_lots_evicted: u64,
+) {
+    emit!(OrderEvictedEvent {
+        market,
+        owner,
+        order_id,
+        side,
+        price,
+        base_lots_evicted,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+
+pub fn emit_order_expired(
+    market: Pubkey,
+    owner: Pubkey,
+    order_id: u64,
+    side: Side,
+    price: u64,
+    base_lots_removed: u64,
+) {
+    emit!(OrderExpiredEvent {
+        market,
+        owner,
+        order_id,
+        side,
+        price,
+        base_lots_removed,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+
+pub fn emit_fee_collected(
+    market: Pubkey,
+    owner: Pubkey,
+    order_id: u64,
+    fees_collected_in_quote_lots: u64,
+) {
+    emit!(FeeCollectedEvent {
+        market,
+        owner,
+        order_id,
+        fees_collected_in_quote_lots,
+        timestamp: Clock::get().unwrap().unix_timestamp,
+    });
+}
+
+pub fn emit_time_in_force(
+    market: Pubkey,
+    order_id: u64,
+    last_valid_slot: u64,
+    last_valid_unix_timestamp_in_seconds: u64,
+) {
+    emit!(TimeInForceEvent {
+        market,
+        order_id,
+        last_valid_slot,
+        last_valid_unix_timestamp_in_seconds,
+    });
 }
