894815dfd6ef77c9dbea14daf8dc66a21a40f7fd
Danish Ansari <dvansari360@gmail.com>
2025-12-27 21:01:51 +0530

    fix:use simple rpc call to place order

--- frontend/api/place-order.ts
+++ frontend/api/place-order.ts
@@ -7,22 +7,20 @@ import {
   useGetOpenOrderPda,
 } from "../services/blockchain";
 import { MARKET_PUBKEY, MAX_BASE_SIZE } from "@/constants/market";
-import { createAssociatedTokenAccountInstruction, getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { BN } from "@coral-xyz/anchor";
-
-import { Transaction } from "@solana/web3.js";
+import { useCreateTokenAccounts } from "@/hooks/useCreateTokenAccounts";
 
 export const placeOrder = () => {
-  const { publicKey, sendTransaction } = useWallet();
+  const { publicKey } = useWallet();
   const { program } = useDexProgram();
-  const { connection } = useConnection();
   const openOrderPda = useGetOpenOrderPda(
     MARKET_PUBKEY,
     publicKey ?? undefined
   );
   const marketInfo = useGetMarketAccount(MARKET_PUBKEY);
-
+  const createTokenAccount = useCreateTokenAccounts()
   return useMutation<any, Error, PlaceOrderInputs>({
     mutationKey: ["placeOrder"],
     mutationFn: async ({
@@ -38,35 +36,19 @@ export const placeOrder = () => {
         if (!openOrderPda) throw new Error("Could not derive open order PDA!");
         if (!program) throw new Error("Program is not initialised!");
 
-        // STEP 1: Ensure token accounts exist
 
-        const baseLotSize = marketInfo.data.baseLotSize.toNumber();
-        const quoteLotSize = marketInfo.data.quoteLotSize.toNumber();
-        // 1 base token = 1000_000 smallest unit
-        const convertedMaxBaseSize = maxBaseSize * MAX_BASE_SIZE;
+        const baseLotSize = marketInfo.data.baseLotSize;
+        const quoteLotSize = marketInfo.data.quoteLotSize;
+
+        const convertedMaxBaseSize = MAX_BASE_SIZE * maxBaseSize
         const priceQuoteLots = Math.floor((price * baseLotSize) / quoteLotSize);
-
-        const tx = new Transaction()
-
-        const baseATA  = await getAssociatedTokenAddress(marketInfo.data.baseMint,publicKey);
-        const quoteAta  = await getAssociatedTokenAddress(marketInfo.data.quoteMint,publicKey)
-        if(!baseATA || !quoteAta){
-          throw new Error("Quote token or base token mint is not found!")
+        const { signature, baseATA, quoteATA } = await createTokenAccount.mutateAsync({ baseMint: marketInfo.data.baseMint, quoteMint: marketInfo.data.quoteMint })
+        console.log("signature:", signature)
+        if (!baseATA || !quoteATA) {
+          throw new Error("Base ATA or Quote ATA not found!")
         }
-        const baseInfo = await connection.getAccountInfo(publicKey);
-        
-        if(!baseInfo){
-          tx.add(
-             createAssociatedTokenAccountInstruction(publicKey,baseATA,publicKey,program.programId)
-          )
-        }
-        const quoteInfo = await connection.getAccountInfo(quoteAta);
-        if(!quoteInfo){
-          tx.add(
-            createAssociatedTokenAccountInstruction(publicKey,quoteAta,publicKey,program.programId)
-          )
-        }
-
+        console.log("base ata:",baseATA.toString());
+        console.log("quote ata:",quoteATA.toString());
         const result = await program.methods
           .placeOrder(
             new BN(convertedMaxBaseSize),
@@ -83,28 +65,17 @@ export const placeOrder = () => {
             baseVault: marketInfo.data.baseVault,
             eventQueue: marketInfo.data.eventQueue,
             userBaseVault: baseATA, // Use the ensured account
-            userQuoteVault: quoteAta, // Use the ensured account
+            userQuoteVault: quoteATA, // Use the ensured account
             market: MARKET_PUBKEY,
             openOrder: openOrderPda,
             tokenProgram: TOKEN_PROGRAM_ID,
           })
-          .instruction();
-          tx.add(result)
-          const {blockhash,lastValidBlockHeight} = await connection.getLatestBlockhash();
-          tx.feePayer = publicKey,
-          tx.recentBlockhash  = blockhash;
-          let signature;
-          try {
-             signature = await sendTransaction(tx,connection,{ skipPreflight: false, maxRetries: 3 });
-          } catch (error) {
-            console.log("error:",error)
-            throw error;
-          }
-          await connection.confirmTransaction({signature,blockhash,lastValidBlockHeight})
-          return signature;
+          .rpc();
       } catch (error) {
+        console.log("error:", error);
         throw error;
       }
-    },
-  });
-};
+      // STEP 1: Ensure token accounts exist
+    }
+  })
+}
