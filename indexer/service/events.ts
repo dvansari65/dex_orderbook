f3363f35f6ac77c997175f913d6a1d6d3c760892
Danish Ansari <dvansari360@gmail.com>
2026-02-09 19:12:03 +0530

    feat:handling onchain events seperately

---
+++ indexer/service/events.ts
@@ -0,0 +1,66 @@
+import { Market } from "../types/market";
+import { Conversion } from "../src/utils/conversion";
+import { Server, Socket } from "socket.io";
+import { handleFillEvent } from "./candle";
+import { OrderFillEventData } from "@/types/events";
+
+export const OrderEvent = async (event: any, io: Server, Market: Market | null, socket: Socket) => {
+    if (!Market) {
+        throw new Error("Market data not found!");
+    }
+    
+    try {
+        const conversion = new Conversion(Market);
+        const converted = conversion.convertEvent(event?.data);
+        
+        // Lightweight event payload
+        const payload = {
+            p: converted.price,
+            q: converted.quantity,
+            ts: converted.timestamp,
+            s: converted.side,
+            id:converted?.orderId
+        };
+        
+        if (event.name === "orderPlacedEvent") {
+            io.emit("order:placed", payload);
+        }
+        if(event.name == "orderFillEvent" || event.name === "orderPartialFillEvent"){
+            const side = event?.data?.side && "bid" in event?.data?.side ? "bid" : "ask"
+
+            const orderFilledEventData:OrderFillEventData = {
+                maker:event?.data?.maker?.toString(),
+                makerOrderId:event?.data?.makerOrderId?.toNumber(),
+                taker:event?.data?.taker?.toString(),
+                takerOrderId:event?.data?.takerOrderId?.toNumber(),
+                side:side,
+                price:event?.data?.price?.toNumber(),
+                baseLotsFilled:event?.data?.baseLotsFilled?.toNumber()/1000,
+                baseLotsRemaining:event?.data?.baseLotsRemaining.toNumber()/1000,
+                timestamp:event?.data?.timestamp.toNumber()
+            }
+            io.emit("order:filled", orderFilledEventData);
+            const fillResult = await handleFillEvent(event?.data, event?.signature);
+            if (!fillResult) {
+                io.emit("error", { message: "Failed to create candle for order fill event!" });
+                return;
+            }
+            // Emit candle data with volume
+            io.emit("candle:filled", {
+                candle: fillResult.candle,
+                volume: fillResult.volume,
+                timestamp: fillResult.timestamp
+            });
+        }
+        if (event.name === "OrderCancelledEvent") {
+            io.emit("order:cancelled", payload);
+        }
+        
+    } catch (error: any) {
+        console.log("Error in OrderEvent:", error);
+        socket.emit("error", { 
+            message: error.message || "Something went wrong!" 
+        });
+        throw error;
+    }
+}
