61b3b0f59ad1d6bb805fdd7c217d3c0b7b124608
Danish Ansari <dvansari360@gmail.com>
2026-04-07 00:33:05 +0530

    refactor: align order enums with updated flow

--- smart_contract/programs/orderbook/src/states/order_schema/enums.rs
+++ smart_contract/programs/orderbook/src/states/order_schema/enums.rs
@@ -1,21 +1,21 @@
 use anchor_lang::prelude::*;
-#[derive(Clone,Copy,PartialEq,Eq,InitSpace,Debug,AnchorSerialize,AnchorDeserialize)]
+#[derive(Clone, Copy, PartialEq, Eq, InitSpace, Debug, AnchorSerialize, AnchorDeserialize)]
 pub enum Side {
     Bid,
-    Ask
+    Ask,
 }
 
 impl Side {
-    pub fn opposite (&self)->Self{
+    pub fn opposite(&self) -> Self {
         match *self {
             Side::Bid => Side::Ask,
-            Side::Ask => Side::Bid
+            Side::Ask => Side::Bid,
         }
     }
-    pub fn from_order_sequence_number (order_id:&u64)->Self{
-        match order_id.leading_zeros(){
+    pub fn from_order_sequence_number(order_id: &u64) -> Self {
+        match order_id.leading_zeros() {
             1 => Side::Bid,
-            _ => Side::Ask
+            _ => Side::Ask,
         }
     }
 }
