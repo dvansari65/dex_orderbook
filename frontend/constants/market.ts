6ae504bfbdcba834b41e4032630f72088ce2e13e
Danish Ansari <dvansari360@gmail.com>
2026-04-07 00:07:44 +0530

    refactor: read market pubkey from shared env config

--- frontend/constants/market.ts
+++ frontend/constants/market.ts
@@ -1,4 +1,6 @@
-export const MARKET_PUBKEY = process.env.NEXT_PUBLIC_MARKET_PUBKEY || "";
+import { MARKET_PUBKEY_ENV } from "@/lib/env";
+
+export const MARKET_PUBKEY = MARKET_PUBKEY_ENV;
 
 export const BASE_TOKEN_DECIMALS=1_000_000;
 export const QUOTE_TOKEN_DECIMALS=1_000_000;
